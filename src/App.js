7550365832143cf3459af6927e7ebedfda42cce9
GlebShylovich <glebshylovich10@gmail.com>
2023-09-26 22:37:14 +0300

    promo div

--- src/App.js
+++ src/App.js
@@ -18,6 +18,22 @@ export default function App() {
             </div>
           </div>
         </header>
+        <main class="page">
+          <div class="page__container">
+            <div class="promo">
+              <div class="promo__container">
+                <h1 class="promo__title">Find your custom matched commercial real estate space.</h1>
+                <p class="promo__subtitle">In oculis quidem exercitus quid ex ea consequatur? autem vel eum fugiat, quo
+                  pertineant ero tibique</p>
+                <div class="promo__action">
+                  <input type="text" class="promo__input" placeholder="Your email here"></input>
+                  <div class="promo__btn">Subscribe</div>
+                </div>
+              </div>
+              <img src="./img/Your image here.png" alt="promo"></img>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
